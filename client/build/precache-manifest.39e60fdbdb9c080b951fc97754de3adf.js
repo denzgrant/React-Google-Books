b25f90f51e7b9db634a08faefbab5f984109e317
Denzell Grant <denzgrant@gmail.com>
2020-08-24 21:08:59 -0700

    build

---
+++ client/build/precache-manifest.39e60fdbdb9c080b951fc97754de3adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49c4037761e6cfd5cdb0",
+    "url": "/static/css/main.ff04ffef.chunk.css"
+  },
+  {
+    "revision": "49c4037761e6cfd5cdb0",
+    "url": "/static/js/main.8bf9a600.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9603bb63c28f335eb790",
+    "url": "/static/js/2.2dde2e74.chunk.js"
+  },
+  {
+    "revision": "0b9666df135d7aa6df81a61bf6dc326d",
+    "url": "/static/media/bookPhoto.0b9666df.JPG"
+  },
+  {
+    "revision": "5262ca1b2da51ff1ee5af21e6714e375",
+    "url": "/index.html"
+  }
+];
