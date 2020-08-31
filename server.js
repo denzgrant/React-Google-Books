d49b457051ac89f80978304909f11b1283586417
Denzell Grant <denzgrant@gmail.com>
2020-08-30 19:49:14 -0700

    updated server

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ app.use(express.json());
 app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-} else {
+  app.use(express.static(__dirname + "/client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname + "./client/build/index.html"));
   });
 }
 
