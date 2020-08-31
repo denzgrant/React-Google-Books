a0b4494794e5811cd6fd86fe6837a5844a964057
Denzell Grant <denzgrant@gmail.com>
2020-08-30 20:25:20 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-
-require('dotenv').config()
-
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Add routes, both API and view
-app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+// Add routes, both API and view
+app.use(routes);
 const uri = (`mongodb+srv://me:${process.env.MONGO_ATLAS_PASS}@googlebooks.i7x4a.mongodb.net/books?retryWrites=true&w=majority`)
 
 mongoose.connect(uri, {
